5c4457be3f7eb6bd834c46c207b21f9b0083d357
smrony <smrony1991@gmail.com>
2021-07-05 20:17:27 +0600

    Laravel V-7.0

---
+++ resources/views/admin/master.blade.php
@@ -0,0 +1,45 @@
+<!doctype html>
+<html lang="en">
+
+    <head>
+        <!-- header -->
+        @include('include.header')
+        <!-- header end -->
+    </head>
+
+    <body>
+        <!-- nav -->
+        @include('include.nav')
+        <!-- nav end -->
+        <div class="container-fluid">
+            <div class="row">
+                <!-- sidebar -->
+                @include('include.sidebar')
+                <!-- sidebar end -->
+                <main class="col-md-9 ms-sm-auto col-lg-10 px-md-4">
+                    <div
+                        class="d-flex justify-content-between flex-wrap flex-md-nowrap align-items-center pt-3 pb-2 mb-3 border-bottom">
+                        <h1 class="h2">Dashboard</h1>
+                        <div class="btn-toolbar mb-2 mb-md-0">
+                            <div class="btn-group me-2">
+                                <button type="button" class="btn btn-sm btn-outline-secondary">Share</button>
+                                <button type="button" class="btn btn-sm btn-outline-secondary">Export</button>
+                            </div>
+                            <button type="button" class="btn btn-sm btn-outline-secondary dropdown-toggle">
+                                <span data-feather="calendar"></span>
+                                This week
+                            </button>
+                        </div>
+                    </div>
+                    <!-- content -->
+                    @yield('site_content')
+                    <!-- content end -->
+                </main>
+            </div>
+        </div>
+        <!-- footer -->
+        @include('include.footer')
+        <!-- footer end  -->
+    </body>
+
+</html>
