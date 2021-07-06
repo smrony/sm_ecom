a0394af0f5a657f34b2eaf01fe25d9e615d1fbf8
smrony <smrony1991@gmail.com>
2021-07-06 21:54:03 +0600

    CRUD files initiated

--- routes/web.php
+++ routes/web.php
@@ -16,3 +16,6 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('admin.master');
 });
+Route::group(['namespace'=>'admin'],function(){
+   Route::resource('categories','CategoryController');
+}); 
