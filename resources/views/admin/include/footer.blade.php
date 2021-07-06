c3b8cf55cc7e686bbdfacbd0e4f3c40037fdce6e
smrony <smrony1991@gmail.com>
2021-07-06 21:20:32 +0600

    js url removed

--- resources/views/admin/include/footer.blade.php
+++ resources/views/admin/include/footer.blade.php
@@ -2,7 +2,7 @@
     <p class="text-center" style="color: #fff;">SM eCOM ~ &copy; 2021</p>
 </footer>
 
-<script src="{ {URL::asset('admin/js/bootstrap.bundle.min.js') } }"></script>
+<script src="{{asset('/admin/js/bootstrap.bundle.min.js') }}"></script>
 
 <script src="https://cdn.jsdelivr.net/npm/feather-icons@4.28.0/dist/feather.min.js"
     integrity="sha384-uO3SXW5IuS1ZpFPKugNNWqTZRRglnUJK6UAZ/gxOX80nxEkN9NcGZTftn6RzhGWE" crossorigin="anonymous">
@@ -10,4 +10,4 @@
 <script src="https://cdn.jsdelivr.net/npm/chart.js@2.9.4/dist/Chart.min.js"
     integrity="sha384-zNy6FEbO50N+Cg5wap8IKA4M/ZnLJgzc6w2NqACZaK0u0FXfOWRRJOnQtpZun8ha" crossorigin="anonymous">
 </script>
-<script src="{ { URL::asset('admin/dashboard.js') } }"></script>
+<script src="{{asset('/admin/dashboard.js') }}"></script>
